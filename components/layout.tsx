a0b1367c0621430a3bed7ab6cc7a8b6828bfcfa5
Gogulaanand R <gogulaanand02@gmail.com>
2021-08-30 19:47:14 +0530

    fix import type

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,7 +4,7 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 import React, {useEffect} from "react";
-import Prism from "Prismjs";
+import Prism from "prismjs";
 
 const name = "Gogulaanand";
 export const siteTitle = "Next.js Sample Website";
--- pages/index.tsx
+++ pages/index.tsx
@@ -22,7 +22,7 @@ export default function Home({ allPostsData }: {allPostsData:{date: string, titl
         <p>This is a place where i pen my thoughts and learnings. <br/>Happy Reading !</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blogs</h2>
+        <h2 className={utilStyles.headingLg}>Posts</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
